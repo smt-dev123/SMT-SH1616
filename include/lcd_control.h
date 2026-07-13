5b33bda9f469cf1389d1c4064e42ed50fa0bf27c
smt-dev123 <luysokmatra1@gmail.com>
2026-07-14 00:20:26 +0700

    change lcd 1602 to 2004

--- include/lcd_control.h
+++ include/lcd_control.h
@@ -4,7 +4,7 @@
 #include <LiquidCrystal_I2C.h>
 #include "config.h"
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 void initLCD()
 {
@@ -12,9 +12,13 @@ void initLCD()
     lcd.backlight();
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("** SMT-SH1616 **");
+    lcd.print("********************");
     lcd.setCursor(0, 1);
-    lcd.print("System Booting..");
+    lcd.print("**** SMT-SH1616 ****");
+    lcd.setCursor(2, 2);
+    lcd.print("System Booting...");
+    lcd.setCursor(0, 3);
+    lcd.print("********************");
     delay(1500);
     lcd.clear();
 }
@@ -22,59 +26,48 @@ void initLCD()
 void updateLCDDisplay()
 {
     static unsigned long lastLCDUpdate = 0;
-    static unsigned long lastPageChange = 0;
-    static int currentPage = 1;
-
-    // ឆ្លាស់ទំព័រ (Page) រាល់ 5 វិនាទីម្តង
-    if (millis() - lastPageChange >= 5000)
-    {
-        lastPageChange = millis();
-        currentPage = (currentPage == 1) ? 2 : 1;
-        lcd.clear(); // សម្អាតអេក្រង់ពេលដូរ Page ការពារអក្សរព្រិច និងជាន់គ្នា
-    }
 
     // ធ្វើបច្ចុប្បន្នភាពទិន្នន័យរាល់ ១ វិនាទីម្តង
     if (millis() - lastLCDUpdate >= 1000)
     {
         lastLCDUpdate = millis();
 
-        if (currentPage == 1)
-        {
-            // ================== PAGE 1: TIME & NETWORK ==================
-            // DateTime now = rtc.now();
-            lcd.setCursor(0, 0);
-            char timeStr[17];
-            snprintf(timeStr, sizeof(timeStr), "TIME: %02d:%02d:%02d", hour(), minute(), second());
-            lcd.print(timeStr);
+        // ================== TIME & NETWORK ==================
+        // DateTime now = rtc.now();
+        lcd.setCursor(0, 0);
+        char timeStr[21];
+        snprintf(timeStr, sizeof(timeStr), "TIME: %02d:%02d:%02d    ", hour(), minute(), second());
+        lcd.print(timeStr);
 
-            lcd.setCursor(0, 1);
-            if (isEthernetConnected)
-            {
-                lcd.print("*NET: ETHERNET");
-            }
-            else if (WiFi.status() == WL_CONNECTED)
-            {
-                lcd.print("*NET: " + WiFi.SSID());
-            }
-            else
-            {
-                lcd.print("*NET: NO NETWORK");
-            }
+        lcd.setCursor(0, 1);
+        if (isEthernetConnected)
+        {
+            lcd.print("NETWORK: ETHERNET   ");
+        }
+        else if (WiFi.status() == WL_CONNECTED)
+        {
+            String netStr = "NETWORK: " + WiFi.SSID();
+            while (netStr.length() < 20)
+                netStr += " ";
+            lcd.print(netStr.substring(0, 20));
         }
         else
         {
-            // ================== PAGE 2: RELAY STATUS (1-16) ==================
-            // ជួរទី១៖ លេខឆានែល (1 ដល់ 9 និង 0=10, 1=11... 6=16)
-            lcd.setCursor(0, 0);
-            lcd.print("1234567890123456");
-
-            // ជួរទី២៖ State 0 ឬ 1 របស់ Relay ទាំង ១៦
-            lcd.setCursor(0, 1);
-            for (int i = 0; i < 16; i++)
-            {
-                lcd.print(ledState[i]);
-            }
+            lcd.print("NETWORK: NO NETWORK ");
         }
+
+        // ================== 2: RELAY STATUS (1-16) ==================
+        // ជួរទី៣៖ លេខឆានែល (1 ដល់ 9 និង 0=10, 1=11... 6=16)
+        lcd.setCursor(2, 2);
+        lcd.print("1234567890123456  ");
+
+        // ជួរទី៤៖ State 0 ឬ 1 របស់ Relay ទាំង ១៦
+        lcd.setCursor(2, 3);
+        for (int i = 0; i < 16; i++)
+        {
+            lcd.print(ledState[i]);
+        }
+        lcd.print("    ");
     }
 }
 
